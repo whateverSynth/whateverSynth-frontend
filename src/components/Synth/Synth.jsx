f5037e4879e3b028ca2a1b123c99e7496afcf6f1
Michael Romay <amerror@gmail.com>
2020-06-30 20:04:46 -0700

    thing

--- src/components/Synth/Synth.jsx
+++ src/components/Synth/Synth.jsx
@@ -346,7 +346,7 @@ export default function Synth() {
         onKeyEvent={(key, e) => keyUp(e)}
       />
 
-      <h1>whateverSynth</h1>
+      <h1>synthinator</h1>
       <Keyboard />
       <Waveshapes />
       <Effects />
