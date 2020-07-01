69a69896944db8f3d06390532f9f7d705690b203
Michael Romay <amerror@gmail.com>
2020-07-01 12:02:38 -0700

    changed volume

--- src/hooks/EffectsProvider.jsx
+++ src/hooks/EffectsProvider.jsx
@@ -5,7 +5,7 @@ const EffectsContext = createContext();
 
 export const EffectsProvider = ({ children }) => {
   const [waveshape, setWaveshape] = useState('sine');
-  const [gainSetting, setGainSetting] = useState(0.8);
+  const [gainSetting, setGainSetting] = useState(0.15);
 
   const [effects, setEffects] = useState([
     // 'Chorus',
@@ -136,7 +136,7 @@ export const EffectsProvider = ({ children }) => {
   };
 
   const handleRemoveEffect = (effectToRemove) => {
-    const newEffects = effects.filter(effect => effect !== effectToRemove);
+    const newEffects = effects.filter((effect) => effect !== effectToRemove);
     setEffects(newEffects);
   };
 
